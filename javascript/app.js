7e7b3ae0139991d434bbd716aac7bf1b950bc896
Rabi Shankar Prasad <82926719+rabiprasad@users.noreply.github.com>
2021-05-10 12:37:18 +0530

    Added all movie fetching, adding functionaliy in content box

--- javascript/app.js
+++ javascript/app.js
@@ -1,24 +1,69 @@
-import endPoint from "./request.js";
-import setGenres from './setGenres.js';
-import {add} from './addContent.js';
+import endPoint from "./endPoint.js";
+import {addContent} from './addContent.js';
 
-//console statement
-console.log('hi');
+//selecting elements from web page.
+const contentBox = document.querySelector('.content-box');
+const categoryBar = document.querySelector('.category-bar');
+const categories = categoryBar.children;
 
+let currentCategory;
+let page = 1;      //page number to fetch movies from specific page
 
-
-
-let currentGenre;
-document.addEventListener('DOMContentLoaded',() => {
-
-    //setGenres.set(endPoint.fetchGenre);
-    const currentGenre = document.querySelector('.genre li[data-genre-id="0"]');
-    currentGenre.classList.add('current-genre')
-
-    add(endPoint.fetchTrending);
+//Adds movies(now_playing i.e. trending) when page is loaded for first time.
+document.addEventListener('DOMContentLoaded', () => {
+    currentCategory = 'now_playing';
+    addContent(endPoint(currentCategory,page++));
+    addContent(endPoint(currentCategory,page++));
+    
+    highlightCurrentCategory(currentCategory);
     //fetchConfig();
 });
 
+//Adds movies when specific category is selected.
+categoryBar.addEventListener('click', e => {
+    contentBox.innerHTML = ``;      //clearing movies of previous category.
+    page = 1;                       //resetting page count to 1.
+
+    currentCategory = e.target.id;
+    addContent(endPoint(currentCategory,page++));
+    addContent(endPoint(currentCategory,page++));
+    highlightCurrentCategory(currentCategory);
+});
+
+//Adds more movies if user reaches to end of page.
+window.addEventListener('scroll',() => {
+    let webPageHeight = document.body.offsetHeight;
+    let ScreenBottom = screen.height+window.pageYOffset;
+    
+    if(ScreenBottom >= webPageHeight){
+        addContent(endPoint(currentCategory,page++));
+        webPageHeight = document.body.offsetHeight;
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+/*helper functions */
+function highlightCurrentCategory(currentCategory){
+    Array.from(categories).forEach(category => {
+        if(category.id === currentCategory){
+            category.classList.add('current-category');
+        }
+        else{
+            category.classList.remove('current-category');
+        }
+    })
+}
+/* development functions */
 function fetchConfig() {
     fetch(endPoint.fetchConfig.url)
     .then(response => {
---
+++ javascript/endPoint.js
@@ -0,0 +1,29 @@
+import {API_KEY} from '../private.js';
+
+/*returns url according to category and page required.
+One page contains limited movie details.
+*/
+function movieCollectionURL(category,page){
+    return `https://api.themoviedb.org/3/movie/${category}?api_key=${API_KEY}&language=en-US&page=${page}`;
+}
+function movieDetailURL(movieId){
+    return `https://api.themoviedb.org/3/movie/${movieId}?api_key=${API_KEY}&language=en-US`;
+   
+}
+function movieSearchURL(query,page){
+    return `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${query}&include_adult=false&page=${page}`;
+}
+function movieProviderURL(movieId){
+    return `https://api.themoviedb.org/3/movie/${movieId}/watch/providers?api_key=${API_KEY}`;
+}
+function companyWebsiteURL(companyId){
+    return `https://api.themoviedb.org/3/company/${companyId}?api_key=${API_KEY}`;
+}
+
+export {
+    movieCollectionURL,
+    movieSearchURL,
+    movieDetailURL,
+    movieProviderURL,
+    companyWebsiteURL
+};
